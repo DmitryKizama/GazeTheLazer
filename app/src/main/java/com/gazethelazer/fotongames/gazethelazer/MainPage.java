b143e8dc9d888b2d8e42db5557a939fb812a3245
Kizama <dmitrikkizama@gmail.com>
2015-07-22 02:58:26 +0300

    added awesome module

--- app/src/main/java/com/gazethelazer/fotongames/gazethelazer/MainPage.java
+++ app/src/main/java/com/gazethelazer/fotongames/gazethelazer/MainPage.java
@@ -1,10 +1,10 @@
 package com.gazethelazer.fotongames.gazethelazer;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
 
 
 public class MainPage extends AppCompatActivity {
