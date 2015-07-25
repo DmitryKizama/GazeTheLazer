ccc156ea8cbcccbdfca0c516fdc17951303bffcb
Kizama <dmitrikkizama@gmail.com>
2015-07-25 13:04:49 +0300

    added matrix snd square

--- app/src/main/java/com/gazethelazer/fotongames/gazethelazer/api/MatrixAPI.java
+++ app/src/main/java/com/gazethelazer/fotongames/gazethelazer/api/MatrixAPI.java
@@ -1,4 +1,59 @@
 package com.gazethelazer.fotongames.gazethelazer.api;
 
+import java.util.Random;
+
 public class MatrixAPI {
+
+    private int[][] matrix;
+    private int widthsM;
+    private int heightsM;
+    private Random random = new Random();
+
+    public MatrixAPI(int widths, int heights) {
+        matrix = new int[widths][];
+        this.heightsM = heights;
+        this.widthsM = widths;
+        for (int i = 0; i < widths; i++) {
+            matrix[i] = new int[heights];
+        }
+
+        for (int i = 0; i < widths; i++) {
+            for (int j = 0; j < heights; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    public int[][] getMatrix(int width, int height) {
+        MatrixAPI m = new MatrixAPI(width, height);
+
+        setRandomMaap();
+
+        return matrix;
+    }
+
+    private void setRandomMaap() {
+        for (int i = 1; i < widthsM; i++) {
+            for (int j = 1; j < heightsM; j++) {
+                matrix[Math.round(widthsM / 6)][Math.round(heightsM / 6)] = 1;
+                matrix[Math.round(widthsM / 6)][Math.round(heightsM / 6) + 1] = 1;
+                matrix[Math.round(widthsM / 6) + 1][Math.round(heightsM / 6) + 1] = 1;
+                if (random.nextBoolean()) {
+                    if (matrix[i - 1][j] == 1)
+                        matrix[i][j] = 1;
+
+                    if (matrix[i][j - 1] == 1)
+                        matrix[i][j] = 1;
+
+                    if (matrix[i][j + 1] == 1)
+                        matrix[i][j] = 1;
+
+                    if (matrix[i + 1][j] == 1)
+                        matrix[i][j] = 1;
+
+                }
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/gazethelazer/fotongames/gazethelazer/api/SquareAPI.java
+++ app/src/main/java/com/gazethelazer/fotongames/gazethelazer/api/SquareAPI.java
@@ -1,4 +1,14 @@
 package com.gazethelazer.fotongames.gazethelazer.api;
 
 public class SquareAPI {
+
+    public boolean right_side = false;
+
+    public boolean bottom_side = false;
+
+    public SquareAPI(boolean rightSide, boolean bottomSide) {
+        right_side = rightSide;
+        bottom_side = bottomSide;
+    }
+
 }
