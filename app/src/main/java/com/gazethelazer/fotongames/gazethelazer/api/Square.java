e5c83d55c01b3e3b6b2e3f6e617785883f0e1ce3
Andrew Kepko <kepkoandrew@gmail.com>
2015-07-28 03:01:10 +0300

    Cleanup & drawing controller draft

--- app/src/main/java/com/gazethelazer/fotongames/gazethelazer/api/Square.java
+++ app/src/main/java/com/gazethelazer/fotongames/gazethelazer/api/Square.java
@@ -1,14 +1,19 @@
 package com.gazethelazer.fotongames.gazethelazer.api;
 
-public class SquareAPI {
+public class Square {
 
     public boolean right_side = false;
 
     public boolean bottom_side = false;
 
-    public SquareAPI(boolean rightSide, boolean bottomSide) {
+    public Square(boolean rightSide, boolean bottomSide) {
         right_side = rightSide;
         bottom_side = bottomSide;
+    }
+
+    public Square()
+    {
+        this(false, false);
     }
 
 }
--- app/src/main/java/com/gazethelazer/fotongames/gazethelazer/controller/ControllerDraw.java
+++ app/src/main/java/com/gazethelazer/fotongames/gazethelazer/controller/ControllerDraw.java
@@ -1,5 +1,21 @@
 package com.gazethelazer.fotongames.gazethelazer.controller;
 
+import com.gazethelazer.fotongames.gazethelazer.api.Square;
+
 public class ControllerDraw {
+
+    int[][][] mRenderedArray;
+
+    public void render()
+    {
+
+    }
+
+    public int[][][] getRenderedArray()
+    {
+        return mRenderedArray;
+    }
+
+    
 
 }
