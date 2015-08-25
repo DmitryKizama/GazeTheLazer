cf632f4894d4c9571a4a864cf13a5481930866da
Kizama <dmitrikkizama@gmail.com>
2015-08-25 20:07:21 +0300

    fixed

--- app/src/main/java/com/gazethelazer/fotongames/gazethelazer/controller/ControllerGame.java
+++ app/src/main/java/com/gazethelazer/fotongames/gazethelazer/controller/ControllerGame.java
@@ -23,14 +23,11 @@ public class ControllerGame {
         field = matrix.getMatrix();
     }
 
-    int calculateScore(Square[][] latestField, Square[][] lastTurnField)
-    {
+    int calculateScore(Square[][] latestField, Square[][] lastTurnField) {
         int score = 0;
 
-        for (int i = 0; i < widht; i++)
-        {
-            for (int j = 0; j < height; j++)
-            {
+        for (int i = 0; i < widht; i++) {
+            for (int j = 0; j < height; j++) {
                 boolean latestSquareBordered, lastTurnSquareBordered;
 
                 latestSquareBordered = checkSquareBorder(latestField, i, j);
@@ -44,32 +41,26 @@ public class ControllerGame {
         return score;
     }
 
-    boolean checkSquareBorder(Square[][] matrix, int x, int y)
-    {
+    boolean checkSquareBorder(Square[][] matrix, int x, int y) {
         boolean left = false, top = false, right = false, bottom = false;
 
-        if (x==0 || matrix[x-1][y]==null || matrix[x][y].bottom_side)
-        {
+        if (x == 0 || matrix[x - 1][y] == null || matrix[x][y].bottom_side) {
             left = true;
         }
-        if (y==0 || matrix[x][y-1]==null || matrix[x][y].right_side)
-        {
+        if (y == 0 || matrix[x][y - 1] == null || matrix[x][y].right_side) {
             top = true;
         }
-        if (x==widht || matrix[x+1][y]==null || matrix[x+1][y].bottom_side)
-        {
+        if (x == widht || matrix[x + 1][y] == null || matrix[x + 1][y].bottom_side) {
             right = true;
         }
-        if (y==height || matrix[x][y+1]==null || matrix[x][y+1].right_side)
-        {
+        if (y == height || matrix[x][y + 1] == null || matrix[x][y + 1].right_side) {
             bottom = true;
         }
 
         return left && top && right && bottom;
     }
 
-    public void setControllerDraw(ControllerDraw controllerDraw)
-    {
+    public void setControllerDraw(ControllerDraw controllerDraw) {
         mControllerDraw = controllerDraw;
     }
 
@@ -121,21 +112,9 @@ public class ControllerGame {
         int iY;
         if (axisX != 0) {
             iX = whileX(x, y, axisX);
-            if (field[iX][y] == null) {
-                whileY(iX, y, 1);
-            }
-            if (field[iX][y - 1] == null) {
-                whileY(iX, y, -1);
-            }
         }
         if (axisY != 0) {
             iY = whileY(x, y, axisY);
-            if (field[iY][y] == null) {
-                whileY(iY, y, 1);
-            }
-            if (field[iY][y - 1] == null) {
-                whileY(iY, y, -1);
-            }
         }
 
         mControllerDraw.render(model);
@@ -154,10 +133,10 @@ public class ControllerGame {
 
     private int whileY(int x, int y, int sign) {
         int i = 0;
-        if (field[x][y] == null)
+        if (field[y][x] == null)
             i = 1;
-        while (field[x][y + i * sign] != null && field[x - 1][y + i * sign] != null) {
-            field[x][y + i * sign].bottom_side = true;
+        while (field[y + i * sign][x] != null && field[y + i * sign][x - 1] != null) {
+            field[y + i * sign][x].bottom_side = true;
             i++;
         }
         return (y + i * sign);
@@ -165,10 +144,10 @@ public class ControllerGame {
 
     private int whileX(int x, int y, int sign) {
         int i = 0;
-        if (field[x][y] == null)
+        if (field[y][x] == null)
             i = 1;
-        while (field[x + i * sign][y] != null && field[x + i * sign][y - 1] != null) {
-            field[x + i * sign][y].right_side = true;
+        while (field[y][x + i * sign] != null && field[y - 1][x + i * sign] != null) {
+            field[y][x + i * sign].right_side = true;
             i++;
         }
         return (x + i * sign);
